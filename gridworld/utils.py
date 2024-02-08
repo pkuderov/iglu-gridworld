40faa5cebdfec17d2c1ce8ae196b84977b4f09e0
Petr Kuderov <pkuderov@gmail.com>
2024-02-09 02:46:18 +0300

    apply numba jit optimization to utils

--- gridworld/utils.py
+++ gridworld/utils.py
@@ -33,35 +33,44 @@ def cube_vertices(x, y, z, n, top_only=False):
     """
     if top_only:
         return [
-            x - n, y + n, z - n, x - n, y + n, z + n, x + n, y + n, z + n, x + n, y + n, z - n,
             # top
+            x - n, y + n, z - n,
+            x - n, y + n, z + n,
+            x + n, y + n, z + n,
+            x + n, y + n, z - n,
         ]
     return [
-        x - n, y + n, z - n, x - n, y + n, z + n, x + n, y + n, z + n, x + n, y + n, z - n,  # top
-        x - n, y - n, z - n, x + n, y - n, z - n, x + n, y - n, z + n, x - n, y - n, z + n,
+        # top
+        x - n, y + n, z - n,
+        x - n, y + n, z + n,
+        x + n, y + n, z + n,
+        x + n, y + n, z - n,
         # bottom
-        x - n, y - n, z - n, x - n, y - n, z + n, x - n, y + n, z + n, x - n, y + n, z - n,  # left
-        x + n, y - n, z + n, x + n, y - n, z - n, x + n, y + n, z - n, x + n, y + n, z + n,  # right
-        x - n, y - n, z + n, x + n, y - n, z + n, x + n, y + n, z + n, x - n, y + n, z + n,  # front
-        x + n, y - n, z - n, x - n, y - n, z - n, x - n, y + n, z - n, x + n, y + n, z - n,  # back
+        x - n, y - n, z - n,
+        x + n, y - n, z - n,
+        x + n, y - n, z + n,
+        x - n, y - n, z + n,
+        # left
+        x - n, y - n, z - n,
+        x - n, y - n, z + n,
+        x - n, y + n, z + n,
+        x - n, y + n, z - n,
+        # right
+        x + n, y - n, z + n,
+        x + n, y - n, z - n,
+        x + n, y + n, z - n,
+        x + n, y + n, z + n,
+        # front
+        x - n, y - n, z + n,
+        x + n, y - n, z + n,
+        x + n, y + n, z + n,
+        x - n, y + n, z + n,
+        # back
+        x + n, y - n, z - n,
+        x - n, y - n, z - n,
+        x - n, y + n, z - n,
+        x + n, y + n, z - n,
     ]
-
-
-@numba.jit(nopython=True, cache=True)
-def cube_normals(top_only=False):
-    if top_only:
-        return [
-            0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,  # top
-        ]
-    return [
-        0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0,  # top
-        0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0,  # bottom
-        -1, 0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0,  # left
-        1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0,  # right
-        0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1,  # front
-        0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, -1,  # back
-    ]
-
 
 @numba.jit
 def normalize(position: tuple[float, float, float]) -> tuple[int, int, int]:
